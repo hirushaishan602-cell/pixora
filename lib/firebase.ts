6fb04fa094a609f2e52a9dd5b2c0fa7642023269
hirushaishan602-cell <hirushaishan602@gmail.com>
2026-09-07 21:16:15 +0530

    update

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLDCkyTs17_DTdvwHEjnbPbDrb_alUoac",
-  authDomain: "pixora-ad6a8.firebaseapp.com",
-  databaseURL: "https://pixora-ad6a8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pixora-ad6a8",
-  storageBucket: "pixora-ad6a8.firebasestorage.app",
-  messagingSenderId: "612800173987",
-  appId: "1:612800173987:web:4292963f3c3825fe86ac16",
+  apiKey: process.env."AIzaSyCLDCkyTs17_DTdvwHEjnbPbDrb_alUoac",
+  authDomain: process.env."pixora-ad6a8.firebaseapp.com",
+  projectId: process.env."pixora-ad6a8",
+  storageBucket: process.env."pixora-ad6a8.firebasestorage.app",
+  messagingSenderId: process.env."612800173987",
+  appId: process.env."1:612800173987:web:4292963f3c3825fe86ac16",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
